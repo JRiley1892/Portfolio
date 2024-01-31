5f7d14ec0983fab08d3d9652ead5d769c7863bdd
JRiley1892 <jakeriley0927@gmail.com>
2024-01-31 21:43:54 +0000

    Added Contact graphic and new projects

--- script.js
+++ script.js
@@ -59,3 +59,13 @@ window.addEventListener("scroll", debounce(checkSlide));
 const button = document.getElementById("button");
 
 button.addEventListener("mouseover", function () {});
+
+//form validation
+const frmvalidator = new validator("contactform");
+frmvalidator.addValidation("name", "req", "Please provide your name");
+frmvalidator.addValidation("email", "req", "Please provide your email");
+frmvalidator.addValidation(
+  "email",
+  "email",
+  "Please enter a valid email address"
+);
